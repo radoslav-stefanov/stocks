f3f79d901f242ac57dd2c50fa9c9fb6caa1c1b9b
Radoslav Stefanov <radoslav@rstefanov.info>
2023-09-10 21:37:16 +0300

    Update.

--- core/portfolio/urls.py
+++ core/portfolio/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('create-portfolio', views.create_portfolio, name='create-portfolio'),
     path('portfolio/<id>/', views.portfolio_detail, name='portfolio-detail'),
     path('portfolio/<int:portfolio_id>/add_stock_transaction/', views.add_stock_transaction, name='add-stock-transaction'),
+    path('portfolio/<int:portfolio_id>/edit_stock_transaction/<int:id>/', views.edit_stock_transaction, name='edit-stock-transaction'),
+    path('portfolio/<int:portfolio_id>/delete_stock_transaction/<int:id>/', views.delete_stock_transaction, name='delete-stock-transaction'),
 ]
--- core/portfolio/views.py
+++ core/portfolio/views.py
@@ -5,6 +5,7 @@ from .models import Portfolio, StockTransaction
 from . models import Portfolio
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.shortcuts import redirect
 from django.db.models import Sum
 from django.shortcuts import get_object_or_404
 
@@ -64,9 +65,20 @@ def portfolio_detail(request, id):
     }
     return render(request, 'portfolio/portfolio-detail.html', context)
 
-def portfolio_delete(request, id):
-    portfolio = Portfolio.objects.get(pk=id)
-    context = {'portfolio': portfolio}
+def edit_stock_transaction(request, portfolio_id, id):
+    transaction = get_object_or_404(StockTransaction, id=id)
+    form = StockTransactionForm(instance=transaction)
+    
+    if request.method == 'POST':
+        form = StockTransactionForm(request.POST, instance=transaction)
+        if form.is_valid():
+            form.save()
+            return redirect('portfolio-detail', id=portfolio_id)
+    
+    context = {'form': form}
+    return render(request, 'portfolio/edit_stock_transaction.html', context)
 
-    portfolio.delete()
-    return render(request, 'portfolio/portfolio-delete.html', context)
+def delete_stock_transaction(request, portfolio_id, id):
+    transaction = get_object_or_404(StockTransaction, id=id)
+    transaction.delete()
+    return redirect('portfolio-detail', id=portfolio_id)
