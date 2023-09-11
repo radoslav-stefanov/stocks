b9d00f6e51acb54186eb340b57ee690691cd6bfc
Radoslav Stefanov <radoslav@rstefanov.info>
2023-09-11 20:59:09 +0300

    Make an update.

--- core/portfolio/views.py
+++ core/portfolio/views.py
@@ -62,14 +62,6 @@ def portfolio_detail(request, id):
         total_cost=Sum('cost')
     )
 
-    for stock in summary:
-        ticker_symbol = stock.get('ticker', None)
-        if ticker_symbol:
-            ticker = yf.Ticker(ticker_symbol)
-            stock['current_price'] = Decimal(ticker.history(period="1d")['Close'].iloc[0])
-            stock['average_price'] = Decimal(stock['total_cost']) / Decimal(stock['total_shares'])
-            stock['p_and_l'] = (stock['current_price'] - stock['average_price']) * Decimal(stock['total_shares'])
-
     context = {
         'portfolio': portfolio,
         'summary': summary,
